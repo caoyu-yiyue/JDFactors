54325e816c441d8c4dfd6d01b28aaa035badc213
Yue <Cy1093662798@outlook.com>
2019-11-05 12:17:19 +0800

    边际分布的参数合并到一个数据框(xts) 中；边际分布参数与copula 参数合并到一个xts 中等
    - 将边际分布参数组成的list 合并到一起，形成一个xts 对象
    - 将边际分布参数的xts 对象与copula 参数的xts 合并。copula 参数向前填充NA 值
    - 在边际分布参数的list 中，只保存需要的前 4 列

--- notebooks/explore/copula_test.R
+++ notebooks/explore/copula_test.R
@@ -11,6 +11,7 @@ library(foreach)
 week_raw <- read_csv("data/raw/csvFiles/week_readed.csv")
 week_use <- filter(week_raw, trdWeek > "2000-01-01")
 week_fac <- as.xts(week_use[, 2:4], order.by = as.Date(week_use$trdWeek))
+year_end_idx <- endpoints(week_fac, on = "year")
 
 
 # garch 模型的指定
@@ -20,7 +21,6 @@ archSpec <- ugarchspec(variance.model = var_mdl, mean.model = mean_mdl, distribu
 
 
 ###### 滚动估计garch ######
-year_end_idx <- endpoints(week_fac, on = "year")
 cluster <- makeCluster(4, type = "FORK")
 rolling_arch <- function(fac_ts) {
   return(ugarchroll(archSpec, fac_ts,
@@ -31,13 +31,18 @@ rolling_arch <- function(fac_ts) {
 }
 arch_roll <- lapply(week_fac, rolling_arch)
 stopCluster(cluster)
+
 arch_roll_dist_df <- lapply(arch_roll, FUN = function(x) {
-  as.data.frame(x, which = "density")[, 1:4]
+  as.data.frame(x, which = "density")
 })
 
 # 保存roll garch 的结果
 # save(arch_roll, arch_roll_dist_df, file = "data/interim/arch_roll_dist.Rda")
+load(file = "data/interim/arch_roll_dist.Rda")
 
+arch_roll_dist_df <- lapply(arch_roll_dist_df, function(x) {
+  x[, 1:4]
+})
 
 
 #### 按年滚动估计copula 系数 #####
@@ -49,12 +54,12 @@ cop_params <- foreach(i = year_end_idx[4:(length(year_end_idx) - 1)], .combine =
   d_window <- week_fac[1:i]
 
   pob_var <- pobs(as.matrix(d_window))
-  cop_mdl <- tCopula(dim = d_dim)
+  cop_mdl <- tCopula(dim = d_dim, dispstr = "un")
   cop_fit <- fitCopula(cop_mdl, pob_var, method = "ml")
 
   # year_dix_last <- format(index(week_fac[year_end_idx[i]]), "%Y")
   year_idx <- index(week_fac[i + 1])
-  copula_params <- xts(t(coef(cop_fit)), year_idx)
+  copula_params <- xts(t(coef(cop_fit)), order.by = year_idx)
 
   return(copula_params)
 }
@@ -67,18 +72,39 @@ stopCluster(cls)
 cpDf2dpDf <- function(df) {
   # 对df 中的每一行进行cp2dp，最后返回一个xts 对象
   dp_matrix <- t(apply(df, 1, FUN = cp2dp, family = "ST"))
-  return(as.xts(dp_matrix))
+  
+  # 确保index 为Date 对象，不包括时间
+  idx <- as.Date(rownames(dp_matrix))
+  return(xts(dp_matrix, order.by = idx))
 }
 # 对arch_roll_dist_df 中的每一个data.farme 进行转换
 roll_dist_dp <- lapply(arch_roll_dist_df, cpDf2dpDf)
 
-# 把不同因子的参数merge 到一起
-names(Reduce(f = function(left, right) {
-  merge.xts(as.xts(left), as.xts(right))
-}, roll_dist_dp))
+
+# 首先对每个因子循环，将其分布参数的名字前加上其自己因子名的前缀
+merge_margin_dist_params <- function(margin_params_list) {
+  # margin_params_list: 保存边际分布参数的list 对象，其中每一个分量是生成一个 margin dist 的参数
+  # 返回一个xts 对象。合并了list 中每一个margin 的dist parameter，以其在list 中的name 为前缀。
+  fac_names <- names(margin_params_list) # 一个名字的charactor vector
+  for (fac in fac_names) {
+    dist_param_names <- names(margin_params_list[[fac]])
+    dist_param_names_with_fac <- paste(fac, dist_param_names, sep = "_")
+    names(margin_params_list[[fac]]) <- dist_param_names_with_fac
+  }
+  # 把不同因子的分布参数merge 到一起
+  merged_margin_dist_params <- Reduce(f = function(left, right) {
+    merge.xts(as.xts(left), as.xts(right))
+  }, x = margin_params_list)
+
+  return(merged_margin_dist_params)
+}
+margin_params_xts <- merge_margin_dist_params(margin_params_list = roll_dist_dp)
 
 
-
+####### merge magin parameters and copula parameters ###########
+merged_params <- merge(margin_params_xts, cop_params, join = "outer")
+# 每一年使用初年估计的copula 参数，所以使用其向前填充
+merged_params <- na.locf(merged_params)
 
 
 
