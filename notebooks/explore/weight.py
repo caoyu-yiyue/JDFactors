4ce97c996ceeef9c3f1c8432d4ac070d00ad491d
Yue <Cy1093662798@outlook.com>
2019-12-03 10:41:48 +0800

    对于mystic 中de 算法的尝试，仍然不好

--- notebooks/explore/weight.py
+++ notebooks/explore/weight.py
@@ -1,15 +1,15 @@
 # %%
 import numpy as np
 import pandas as pd
+# from mystic import termination
 from mystic.penalty import linear_equality, linear_inequality
 from mystic.solvers import diffev2
 from mystic.strategy import Rand1Bin
 from mystic.symbolic import (generate_conditions, generate_constraint,
                              generate_penalty, generate_solvers, simplify)
+# from mystic.pools import SerialPool as Pool
 
 # from mystic.tools import random_seed
-
-# from scipy.optimize import Bounds, minimize
 
 # %%
 random_num: pd.DataFrame = pd.read_feather('data/interim/random_num.feather')
@@ -106,128 +106,30 @@ cf = generate_constraint(generate_solvers(simplify(equation)))
 bounds = [(0, 1)] * FAC_NUM
 # random_seed(101)
 
-
 # %%
 result = diffev2(cost=expect_value,
                  args=(rf, 7, first_array),
                  x0=bounds,
                  bounds=bounds,
-                 gtol=10,
-                 scale=0.1,
+                 gtol=200,
+                 scale=0.8,
                  constraint=cf,
                  penalty=pf,
                  strategy=Rand1Bin,
                  full_output=True,
-                 npop=1000)
+                 npop=800)
 result
 
 # %%
-# # %%
-# def jacob(weight: np.array, rf, gamma, fac_array: np.ndarray):
-#     """
-#     针对目标函数计算梯度向量
-
-#     Parameter:
-#     ----------
-#     weight:
-#         np.array
-#         权重array
-#     rf:
-#         float
-#         下一期的无风险收益
-#     gamma:
-#         float
-#         CRRA 中的gamma
-#     fac_df:
-#             np.ndarray
-#             只保存因子的ndArray，不包含时间列
-
-#     Result:
-#     -------
-#     np.array
-#         表示梯度向量的np.array
-#     """
-
-#     weight = np.asarray(weight)
-#     # 计算每个导数的共同部分，命名为gradient_core
-#     gradient_core: np.array = (1 + rf + fac_array.dot(weight))**(-gamma)
-#     # 在每点（随机数）上，每个梯度分量，是共同部分乘以相应的因子值。
-#     gradient_array: np.ndarray = fac_array * gradient_core[:, np.newaxis]
-
-#     # 最终在每列上求平均，得到目标函数的梯度向量。
-#     return gradient_array.mean(axis=0)
-
-# # %%
-# def hess(weight: np.array, rf, gamma, fac_array: np.ndarray):
-#     """
-#     针对目标函数计算梯度向量
-
-#     Parameter:
-#     ----------
-#     weight:
-#         np.array
-#         权重array
-#     rf:
-#         float
-#         下一期的无风险收益
-#     gamma:
-#         float
-#         CRRA 中的gamma
-#     fac_df:
-#             np.ndarray
-#             只保存因子的ndArray，不包含时间列
-
-#     Result:
-#     -------
-#     np.ndarray
-#         一个2d array，是对 weight 向量的海塞矩阵，
-#     """
-
-#     weight = np.asarray(weight)
-#     # 首先计算每个海塞矩阵分量需要的共同部分，每一个随机数对应一个，形成一个array
-#     hass_core = -gamma * (1 + rf + fac_array.dot(weight))**(-gamma - 1)
-
-#     # 计算海塞矩阵分量的后半部分，是fac_array 每一行的向量外积。
-#     # 为了使每一行的计算自动归为一个ndarray，将fac_array 升到 3 维，
-#     # 乘号前面的分量是一个列向量，后面的分量为行向量，最终在每个分量处对应相乘，得到向量的外积。
-#     outer_product_array = fac_array[:, :, np.newaxis] * fac_array[:, np.
-#                                                                   newaxis, :]
-#     # 每个hass_core 分量乘对应的外积矩阵分量。这里将hass_core 升到 3 维相乘即可。
-#     hass_matr_array: np.ndarray = outer_product_array * hass_core[:, np.
-#                                                                   newaxis,
-#                                                                   np.newaxis]
-
-#     # 最终计算每个随机数形成的海塞矩阵的平均值，得到原 objective 函数的海塞矩阵
-#     return hass_matr_array.mean(axis=0)
-
-# %%
-# 限制条件
-# def weight_constrain(weight, mr):
-#     """
-#     对权重的和施加限制条件，
-#     Parameters:
-#     -----------
-#     weight:
-#         list
-#         权重序列
-#     mr:
-#         float
-#         杠杆率
-#     """
-#     return (1 / mr) - weight[0] - (2 * np.sum(weight[1:]))
-
-# con = {'type': 'ineq', 'fun': weight_constrain, 'args': (0.2, )}
-
-# # %%
-# bnd = Bounds([0] * (FAC_NUM - 1), [1] * (FAC_NUM - 1))
-
-# # %%
-# sol = minimize(fun=expect_value,
-#                x0=[0., 0., 0.],
-#                args=(rf, 7, first_array),
-#                method='trust-constr',
-#                jac=jacob,
-#                hess=hess,
-#                constraints=con,
-#                bounds=bnd)
-# sol
+# diff_solver = DifferentialEvolutionSolver2(dim=FAC_NUM, NP=40)
+# diff_solver.SetConstraints(cf)
+# diff_solver.SetPenalty(pf)
+# diff_solver.SetStrictRanges(min=[0] * FAC_NUM, max=[1] * FAC_NUM)
+# diff_solver.SetInitialPoints(x0=)
+# term = termination.CollapseAt(generations=100)
+# diff_solver.SetTermination(termination=term)
+# diff_solver.SetMapper(Pool().map)
+# diff_solver.Solve(cost=expect_value,
+#                   ExtraArgs=(rf, 7, first_array),
+#                   ScalingFactor=0.7, disp=True)
+# diff_solver.Solution()
