42e7128c3ec707a15ec50025e6924315b8b4ce5c
Yue <Cy1093662798@outlook.com>
2020-04-22 19:21:17 +0800

    对整体数据进行copula 的函数 - 目前完成了一个对输入数据进行copula 的功能函数。可以对整体或滚动时调用

---
+++ src/process/copula_mdl.R
@@ -0,0 +1,74 @@
+#!/usr/bin/env Rscript
+
+#############################################################################
+# 拟合copula 模型的函数功能。
+# 独立运行脚本时将计算出数据整体的copula 对象
+#############################################################################
+
+suppressPackageStartupMessages({
+  library(rmgarch)
+})
+
+source("src/process/multi_garch_mdl.R")
+source("src/data/read_data.R")
+
+
+# uarch_spec 输入换成multigarch spec 对象
+fit_garch_copula <- function(multigarch_spec, fac_data, copula_type, is_dcc,
+                             asymm = FALSE, multigarch_fit = NULL,
+                             use_cluster = TRUE) {
+  #' @title 基于multigarch_spec 对象和一组数据，计算出一个copula fit 对象
+  #'
+  #' @param multigarch_spec 一个multigarch spec 对象，将传递给 cgarchspec(uspec)
+  #' @param fac_data xts 等对象。用于计算模型的数据（xts 等对象）
+  #' @param copula_type str, c("mvnorm", "mvt") 的其中之一。copula 模型的类型。
+  #' @param is_dcc Bool. 是否使用dcc 即time.varying。
+  #' @param asymm Bool, default FALSE. 是否使用非对称的dcc 模型。
+  #' @param multigarch_fit (optional) uGARCHmultifit 对象，default NULL。
+  #' 已经拟合过的multifit 对象。
+  #' @param use_cluster Bool. 是否使用cluster(fork cluster) 进行并行计算
+  #'
+  #' @details 前面的5 个参数均传递给rmgarch::cgarchspec(), 最后两个传递给rmgarch::cgarchfit()
+  #' @return cGARCHfit 对象。根据上述的参数拟合完成的cGARCHfit 对象。
+
+  # 指定coplua spec
+  cop_spec <- cgarchspec(
+    uspec = multigarch_spec,
+    asymmetric = asymm,
+    distribution.model = list(
+      copula = copula_type, method = "ML",
+      time.varying = is_dcc, transformation = "parametric"
+    )
+  )
+
+  # 进行copula fit
+  cls <- if (use_cluster) parallel::makeForkCluster(nnodes = 4) else NULL
+  cop_fit <- cgarchfit(cop_spec,
+    data = fac_data, cluster = cls,
+    fit = multigarch_fit
+  )
+  if (use_cluster) stopCluster(cls)
+
+  return(cop_fit)
+}
+
+
+copula_all_main <- function() {
+  #' @title 计算整体数据多种copula 对象的主函数
+
+  multigarch_fit <- read_multi_garch_fit()
+  best_arma_order <- read_best_arma_order()
+  facs_xts <- read_fac_xts()
+  multi_garch_spec <- all_facs_multigarch(
+    arma_order_df = best_arma_order,
+    fit = FALSE
+  )
+
+  static_norm_cop <- fit_garch_copula(
+    multigarch_spec = multi_garch_spec,
+    fac_data = facs_xts,
+    copula_type = "mvnorm",
+    is_dcc = FALSE,
+    multigarch_fit = multigarch_fit
+  )
+}
