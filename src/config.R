18e8507b75ce4f4307f83f8e59c3d00edbcdc0bd
Yue <Cy1093662798@outlook.com>
2020-06-02 12:46:36 +0800

    日数据的Step 更改为252

--- src/config.R
+++ src/config.R
@@ -2,6 +2,6 @@
 # 一些项目中需要使用的常量
 #############################################################################
 
-ROLLING_STEP <- c(Week = 52, Day = 250, Month = 12)
+ROLLING_STEP <- c(Week = 52, Day = 252, Month = 12)
 # 对于Month，这里使用的是行号，而非年数，用于精细控制起始行
 IN_SAMPLE_YEARS <- c(Week = 5, Day = 5, Month = 100)
