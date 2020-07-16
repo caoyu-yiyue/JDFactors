d5bfd9d46b169dd5fe10e1fdf0093d65398282b7
Yue <Cy1093662798@outlook.com>
2020-07-16 12:15:32 +0800

    rf 数据在最开始就转换为小数

--- src/data/rf_data.R
+++ src/data/rf_data.R
@@ -12,5 +12,7 @@ rf_df <- read.csv("data/raw/risk_free.csv",
 )
 
 rf_xts <- xts::as.xts(rf_df[, 2:ncol(rf_df)], order.by = as.Date(rf_df[, 1]))
+# 原始数据是百分比，需要转换成小数
+rf_xts <- rf_xts / 100
 colnames(rf_xts) <- c("Year_rf", "Day_rf", "Week_rf", "Month_rf")
 saveRDS(rf_xts, "data/interim/rf_xts.Rds")
