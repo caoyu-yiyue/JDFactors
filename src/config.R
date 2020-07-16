f8e4b2d582472f881c26eec2793bcb8e6dda8c08
Yue <Cy1093662798@outlook.com>
2020-07-16 20:34:57 +0800

    恢复默认的step 为52，start 5；rolling garch order 为1，1

--- src/config.R
+++ src/config.R
@@ -2,9 +2,9 @@
 # 一些项目中需要使用的常量
 #############################################################################
 
-ROLLING_STEP <- c(Week = 12, Day = 60, Month = 12)
+ROLLING_STEP <- c(Week = 52, Day = 252, Month = 12)
 # 对于Month，这里使用的是行号，而非年数，用于精细控制起始行
-IN_SAMPLE_YEARS <- c(Week = 8, Day = 8, Month = 100)
+IN_SAMPLE_YEARS <- c(Week = 5, Day = 5, Month = 100)
 
 ROLLING_ARMA_ORDERS <- matrix(rep(3, 10), nrow = 2)
-ROLLING_GARCH_ORDERS <- matrix(rep(2, 10), nrow = 2)
+ROLLING_GARCH_ORDERS <- matrix(rep(1, 10), nrow = 2)
